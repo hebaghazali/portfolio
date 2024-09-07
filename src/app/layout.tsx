747f1d8a466daefce5eacaf64a8339cdc1af7c61
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2024-09-07 12:02:50 +0300

    feat: add links for logos in homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 
 import { Nav } from '@/components/nav';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 import ClientLayout from '../components/clientLayout';
 import styles from './layout.module.css';
@@ -35,7 +36,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang='en'>
             <body>
                 <header className={styles.header}>
-                    <Image src='/images/logo.svg' alt='Logo' width={80} height={80} priority />
+                    <Link href='/'>
+                        <Image src='/images/logo.svg' alt='Logo' width={80} height={80} priority />
+                    </Link>
                     <Nav mode='light' />
                 </header>
                 <Suspense fallback={<div>Loading...</div>}>
@@ -44,21 +47,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
                 <footer className={styles.footer}>
                     <div className={styles.footerNav}>
-                        <Image src='/images/logo-white.svg' alt='Logo' width={80} height={80} priority />
+                        <Link href='/'>
+                            <Image src='/images/logo-white.svg' alt='Logo' width={80} height={80} priority />
+                        </Link>
                         <Nav mode='dark' />
                     </div>
                     <div className={styles.socials}>
-                        <Image src='/images/icons/linkedin-white.svg' alt='Logo' width={24} height={24} priority />
-                        <Image src='/images/icons/github-white.svg' alt='Logo' width={24} height={24} priority />
+                        <Link href='https://www.linkedin.com/in/hebaghazali/' target='_blank'>
+                            <Image src='/images/icons/linkedin-white.svg' alt='Logo' width={24} height={24} priority />
+                        </Link>
+                        <Link href='https://github.com/hebaghazali' target='_blank'>
+                            <Image src='/images/icons/github-white.svg' alt='Logo' width={24} height={24} priority />
+                        </Link>
                         <p className={`body-3 ${styles.copyright}`}>
                             Developed by Heba Ghazali <br />
                             Design inspired by{' '}
-                            <a
+                            <Link
                                 href='https://www.frontendmentor.io/challenges/minimalist-portfolio-website-LMy-ZRyiE'
                                 target='_blank'
                             >
                                 FrontendMentor.io
-                            </a>
+                            </Link>
                         </p>
                     </div>
                 </footer>
